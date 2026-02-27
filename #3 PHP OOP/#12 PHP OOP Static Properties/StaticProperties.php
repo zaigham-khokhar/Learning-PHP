1f154825db7f9dd8912eb20a8b43cf0ec64590ae
zaigham-khokhar <learningreact474676@gmail.com>
2026-02-27 15:16:18 +0500

    Add example code for declaring and accessing static properties in PHP

--- #3 PHP OOP/#12 PHP OOP Static Properties/StaticProperties.php
+++ #3 PHP OOP/#12 PHP OOP Static Properties/StaticProperties.php
@@ -4,4 +4,14 @@
     Static properties can be accessed directly - without creating an instance of a class first. */
 
     # Declare a Static Property
+    // To declare a static property in a class, use the static keyword:
+    # Here is Code Example:
+    class ClassName {
+        public static $statProp = "Hey I am Zaigham";
+    }
+
+    # Access a Static Property
+    /* To access a static property, specify the class name, followed by a
+    double colon (::) and the method name: */
+    ClassName::$statProp;
 ?>
