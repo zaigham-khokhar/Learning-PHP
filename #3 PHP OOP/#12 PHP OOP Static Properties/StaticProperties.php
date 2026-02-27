5ab33123d285d0677bd87cf94cb5bb4e7e82e3da
zaigham-khokhar <learningreact474676@gmail.com>
2026-02-27 15:09:51 +0500

    Add comments explaining static properties in PHP

--- #3 PHP OOP/#12 PHP OOP Static Properties/StaticProperties.php
+++ #3 PHP OOP/#12 PHP OOP Static Properties/StaticProperties.php
@@ -1,3 +1,7 @@
 <?php
     # PHP - Static Properties
+    /* The static keyword is used to create static properties.
+    Static properties can be accessed directly - without creating an instance of a class first. */
+
+    # Declare a Static Property
 ?>
