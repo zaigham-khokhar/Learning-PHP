6604c562afe97e2685b875ee62394ea75e228a1b
zaigham-khokhar <learningreact474676@gmail.com>
2026-02-27 15:21:13 +0500

    Add comments explaining static properties in MoreOnStaticProperties.php

--- #3 PHP OOP/#12 PHP OOP Static Properties/MoreOnStaticProperties.php
+++ #3 PHP OOP/#12 PHP OOP Static Properties/MoreOnStaticProperties.php
@@ -1,3 +1,8 @@
 <?php
     # PHP - More On Static Properties
+    /* A class can have both static and non-static properties.
+    A static property can be accessed from a method in the same class
+    using the self keyword and double colon (::): */
+    # Here is Code Example:
+    
 ?>
