71e4b726d49a9844cfc96647d547d484216bee4a
zaigham-khokhar <learningreact474676@gmail.com>
2026-02-27 15:35:07 +0500

    Add detailed examples for declaring and accessing static properties in PHP

--- #3 PHP OOP/#12 PHP OOP Static Properties/MoreOnStaticProperties.php
+++ #3 PHP OOP/#12 PHP OOP Static Properties/MoreOnStaticProperties.php
@@ -4,5 +4,32 @@
     A static property can be accessed from a method in the same class
     using the self keyword and double colon (::): */
     # Here is Code Example:
-    
+    class pi {
+        public static $value=3.14159;
+        public function staticValue() {
+            return self::$value;
+        }
+    }
+    $pi = new pi();
+    echo $pi->staticValue();
+
+    /* To call a static property from a child class, use the parent keyword
+    inside the child class: */
+    # Here is Code Example:
+    class pi {
+        public static $value=3.14159;
+    }
+
+    class x extends pi {
+        public function xStatic() {
+            return parent::$value;
+        }
+    }
+
+    // Get value of static property directly via child class
+    echo x::$value;
+
+    // or get value of static property via xStatic() method
+    $x = new x();
+    echo $x->xStatic();
 ?>
