c13de7a48da421d765ee5aa6fe4a85b86937e797
zaigham-khokhar <learningreact474676@gmail.com>
2026-02-27 15:43:33 +0500

    Add PHP NameSpaces example with explanatory comments

---
+++ #3 PHP OOP/#13 PHP OOP NameSpaces/NameSpaces.php
@@ -0,0 +1,7 @@
+<?php
+    # PHP NameSpaces
+    /* PHP namespaces are used to prevent naming conflicts between classes, interfaces,
+    functions, and constants.
+    Namespaces are used to group related code together under a name - to avoid naming
+    conflicts when your code grows, or when you use code from multiple sources. */
+?>  
