db8949e754a8711a0ca9bb93a591fccb1ac82805
zaigham-khokhar <learningreact474676@gmail.com>
2026-03-03 13:44:17 +0500

    Implement basic functionality for Todo List application with task addition, deletion, and editing features.

--- #5 PHP TODO-LIST Mini Project/index.php
+++ #5 PHP TODO-LIST Mini Project/index.php
@@ -1,3 +1,61 @@
 <?php
-    
+session_start();
+require "Todo.php";
+
+$todo = new Todo();
+
+// Add Task Condition
+if (isset($_POST["task"]) && !isset( $_POST["edit_index"])) {
+    $todo->addTask($_POST["task"]);
+}
+
+// Delete Task Condition
+if (isset($_GET["delete"])) {
+    $todo->deleteTask($_GET["delete"]);
+}
+
+// Update Task Condition
+if (isset($_POST["edit_index"])) {
+    $todo->editTask($_POST["edit_index"], $_POST["task"]);
+}
+
+$tasks = $todo->getTasks();
+
+// Edit Task Condition
+$editTask = "";
+$editIndex = null;
+if (isset($_GET["edit"])) {
+    $editIndex = $_GET["edit"];
+    $editTask = $tasks[$editIndex];
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini-Todo-Project-Developed-By-Zaigham</title>
+</head>
+<body>
+    <h2>Todo List</h2>
+    <form method="POST">
+        <input type="text" name="task" id="Task-Input" placeholder="Enter Task" required>
+        <?php if($editIndex !== null){ ?>
+            <input type="hidden" name="edit_index" value="<?php echo $editIndex; ?>">
+            <button>Update</button>
+        <?php } else { ?>
+        <button>Add</button>
+        <?php } ?>
+    </form>
+    <ul>
+        <?php foreach($tasks as $index => $task){ ?>
+            <li>
+                <?php echo $task; ?>
+                <a href="?edit<?php echo $index; ?>">Edit</a>
+                <a href="?delete<?php echo $index; ?>">Delete</a>
+            </li>
+    <?php } ?>
+    </ul>
+</body>
+</html>
