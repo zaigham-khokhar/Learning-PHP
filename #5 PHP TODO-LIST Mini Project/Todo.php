d6b128e5e4c7513fb4f0004dcec8d6435d9dda54
zaigham-khokhar <learningreact474676@gmail.com>
2026-03-03 14:00:15 +0500

    Add Todo class with constructor to initialize session tasks

--- #5 PHP TODO-LIST Mini Project/Todo.php
+++ #5 PHP TODO-LIST Mini Project/Todo.php
@@ -1,3 +1,17 @@
 <?php
-    
+    class Todo {
+        public function __construct() {
+            if(isset($_SESSION["tasks"])) {
+                $_SESSION["tasks"] = [];
+            }
+        }
+
+        // Add Task Function
+
+        // Get All Tasks Function
+
+        // Delete Task Function
+
+        // Edit Task Function
+    }
 ?>
