13bf4352c152b63bad28e1758fe41e026b51e688
zaigham-khokhar <learningreact474676@gmail.com>
2026-03-03 14:13:59 +0500

    Refactor Todo class constructor to properly manage session initialization and tasks array

--- #5 PHP TODO-LIST Mini Project/Todo.php
+++ #5 PHP TODO-LIST Mini Project/Todo.php
@@ -1,31 +1,38 @@
 <?php
-    // Make a Class Names Todo
-    class Todo {
-        session_start();
-        public function __construct() {
-            if(isset($_SESSION["tasks"])) {
-                $_SESSION["tasks"] = [];
-            }
+// Make a Class Named Todo
+class Todo {
+    public function __construct() {
+        // Start the session inside the constructor
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
         }
 
-        // Add Task Function
-        public function addTask($task){
-            $_SESSION["tasks"][] = $task;
-        }
-
-        // Get All Tasks Function
-        public function getTask(){
-            return $_SESSION["tasks"];
-        }
-
-        // Delete Task Function
-        public function deleteTask($index){
-            unset($_SESSION["tasks"][$index]);
-        }
-
-        // Edit Task Function
-        public function editTask($index, $newTask){
-            $_SESSION["tasks"][$index] = $newTask;
+        // Initialize tasks array if not already set
+        if (!isset($_SESSION["tasks"])) {
+            $_SESSION["tasks"] = [];
         }
     }
+
+    // Add Task Function
+    public function addTask($task) {
+        $_SESSION["tasks"][] = $task;
+    }
+
+    // Get All Tasks Function
+    public function getTask() {
+        return $_SESSION["tasks"];
+    }
+
+    // Delete Task Function
+    public function deleteTask($index) {
+        unset($_SESSION["tasks"][$index]);
+        // Reindex the array to avoid gaps
+        $_SESSION["tasks"] = array_values($_SESSION["tasks"]);
+    }
+
+    // Edit Task Function
+    public function editTask($index, $newTask) {
+        $_SESSION["tasks"][$index] = $newTask;
+    }
+}
 ?>
