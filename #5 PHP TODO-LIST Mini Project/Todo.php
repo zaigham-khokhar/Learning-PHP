e6465840a17da1bbeced2c9539a1e70d6a79f9d5
zaigham-khokhar <learningreact474676@gmail.com>
2026-03-03 14:09:28 +0500

    Refactor Todo class to include task management methods: addTask, getTask, deleteTask, and editTask

--- #5 PHP TODO-LIST Mini Project/Todo.php
+++ #5 PHP TODO-LIST Mini Project/Todo.php
@@ -1,4 +1,5 @@
 <?php
+    // Make a Class Names Todo
     class Todo {
         public function __construct() {
             if(isset($_SESSION["tasks"])) {
@@ -7,11 +8,23 @@
         }
 
         // Add Task Function
+        public function addTask($task){
+            $_SESSION["tasks"][] = $task;
+        }
 
         // Get All Tasks Function
+        public function getTask(){
+            return $_SESSION["tasks"];
+        }
 
         // Delete Task Function
+        public function deleteTask($index){
+            unset($_SESSION["tasks"][$index]);
+        }
 
         // Edit Task Function
+        public function editTask($index, $newTask){
+            $_SESSION["tasks"][$index] = $newTask;
+        }
     }
 ?>
